db0aa60606f3a05d4cb89928ccfd9c1c20c0ab53
Miogun <75124706+Miogun@users.noreply.github.com>
2023-11-16 16:25:32 +0900

    Add files via upload

---
+++ scripts/after_install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd   /home/ubuntu/hello-flask
+
+echo ">>> pip install ----------------------"
+pip install -r requirements.txt
+
+echo ">>> run app --------------------------"
+nohup python3 app.py &
+
+echo ">>> remove template files ------------"
+rm -rf appspec.yml requirements.txt
+
+echo ">>> change owner to ubuntu -----------"
+chown -R ubuntu /home/ubuntu/hello-flask
